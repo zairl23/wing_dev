7525689f02284cc782e6d2cf6465180b8eb74495
zairl23 <756127792@qq.com>
2011-09-11 18:46:43 +0800

    init wing_dev

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WingDev::Application.config.secret_token = '7e8b9479759c0ad8f36d3046933b1c5ce6d309c019f8f0f4ad792ecbd41cac6da354f00f16674b53587a5ae8917caa8ecfd5abdec882323a09e98afe30d830f4'
