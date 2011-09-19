79e97aef3f6df904648f1c5b3b39ae420e77599a
zairl23 <756127792@qq.com>
2011-09-19 22:19:52 +0800

    add column url to books

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110916051707) do
+ActiveRecord::Schema.define(:version => 20110919135656) do
 
   create_table "autors", :force => true do |t|
     t.string   "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20110916051707) do
     t.text     "summary"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "url"
   end
 
   create_table "rails_admin_histories", :force => true do |t|
