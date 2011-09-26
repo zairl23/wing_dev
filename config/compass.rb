e1fed5cc9a5b9f7f274fcd1bcd6f8efd3fbf3924
zairl23 <756127792@qq.com>
2011-09-26 17:58:16 +0800

    config compass for heroku

--- config/compass.rb
+++ config/compass.rb
@@ -22,4 +22,10 @@ http_path = "/"
 # sass-convert -R --from scss --to sass app/stylesheets scss && rm -rf sass && mv scss sass
 
 # Changing the output directory of the resulting CSS file
-  css_dir = "app/assets/stylesheets/"
+if Rails.env.production?
+  css_dir = "tmp/stylesheets/compiled"
+  sass_dir = 'app/views/stylesheets'
+else
+  css_dir = "public/stylesheets/"
+  sass_dir = 'app/stylesheets'
+end
