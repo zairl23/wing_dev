1cda72e6a9833094acef1e4bf79bfedbba2cbeb2
zairl23 <756127792@qq.com>
2011-10-04 12:48:41 +0800

    add something to compass

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,3 +1,7 @@
+require 'compass'
+require 'compass/app_integration/rails'
+Compass::AppIntegration::Rails.initialize!
+
 if Compass::AppIntegration::Rails.env == "production"
   require 'fileutils'
   FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets", "compiled"))
